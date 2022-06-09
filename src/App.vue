835f33d4d19af621fa69609b8f7285df689cdaf1
Louis <ls.bouet@gmail.com>
2022-06-09 11:36:05 +0200

    create clone insta

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Mon clone Instagram</h1>
+    <div class="card-container" >
+      <div v-for="post in postList" :key="post.id">
+        <CardComponent :post="post" v-on:like="like"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardComponent from './components/CardComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CardComponent
+  },
+  data() {
+    return {
+      postList: [
+        {
+          "id": "1",
+          "title": "Ma super app vuejs !",
+          "content": "C'était un super projet",
+          "imgUrl": "https://picsum.photos/200/300",
+          "like": 0
+        },
+        {
+          "id": "2",
+          "title": "Ma super app react !",
+          "content": "C'était un autre super projet",
+          "imgUrl": "https://picsum.photos/200/300",
+          "like": 0
+        },
+        {
+          "id": "3",
+          "title": "Ma super app angular !",
+          "content": "Encore un super projet",
+          "imgUrl": "https://picsum.photos/200/300",
+          "like": 0
+        },
+        {
+          "id": "4",
+          "title": "Mon super Wordpress !",
+          "content": "Pas fou ça",
+          "imgUrl": "https://picsum.photos/200/300",
+          "like": 0
+        }
+      ]
+    }
+  },
+  methods: {
+    like(post) {
+      post.like++;
+    }
   }
 }
 </script>
@@ -19,8 +64,16 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.card-container{
+  display: flex;
+  /* flex-direction: row; */
+  flex-wrap: wrap;
+  justify-content: space-around;
+  width: 100%;
+}
+h1{
+  text-align: center;
 }
 </style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="card">
+        <div class="card-img">
+            <img :src="post.imgUrl" alt="">
+        </div>
+        <h1>{{ post.title }}</h1>
+        <p>{{ post.content }}</p>
+        <div>
+            <button v-on:click="() => this.$emit('like', post)">
+                <span>{{ post.like }} {{ post.like > 1 ? 'Likes' : 'Like' }}</span>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'CardComponent',
+    props:['post']
+}
+</script>
+
+<style scoped>
+.card {
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+    height: auto;
+    align-items: center;
+    border-radius: 6px;
+    box-shadow: 0px 0px 4px 0px #000000;
+    margin: 20px;
+    padding: 10px;
+}
+
+.card-img{
+    width: 200px;
+    height: 200px;
+}
+
+.card-img img{
+    width: 100%;
+    height: 100%;
+}
+
+button{
+    background-color: #00bcd4;
+    border: none;
+    border-radius: 6px;
+    color: white;
+    padding: 10px;
+    margin: 10px;
+    cursor: pointer;
+}
+
+</style>
